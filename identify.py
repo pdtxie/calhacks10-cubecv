d147c8359b6ed0a70683e64a36e29307e83b3e8d
Tim Xie <pdt.xie@gmail.com>
2023-10-28 23:47:54 -0700

    clean up

--- identify.py
+++ identify.py
@@ -1,38 +1,46 @@
 import cv2 
 import numpy as np 
 
-green = [[43, 153, 0], [132, 255, 255], 'green']
-blue = [[100, 143, 145], [118, 255, 255], 'blue']
 
-orange = [[0, 172, 83], [20, 255, 255], 'orange']
-red = [[170, 145, 80], [179, 255, 255], 'red']
+USE_CAM = False
+cap = None
 
-# white = [[60, 60, 215], [120, 115, 255], 'white']
-yellow = [[28, 145, 0], [40, 255, 255], 'yellow']
+GREEN = [[43, 153, 0], [132, 255, 255]]
+BLUE = [[100, 143, 145], [118, 255, 255]]
 
-ranges = [green, blue, orange, red, yellow]
+ORANGE = [[0, 172, 83], [20, 255, 255]]
+RED = [[170, 145, 80], [179, 255, 255]]
 
+YELLOW = [[28, 145, 0], [40, 255, 255]]
 
-USE_CAM = True
+RANGES = [GREEN, BLUE, ORANGE, RED, YELLOW]
+
 
 if USE_CAM:
     cap = cv2.VideoCapture(1)
     waitTime = 330
-else:
-    img = cv2.imread('media/test_3.jpg')
 
 
-while (True):
+def get_image():
     if USE_CAM:
-        ret, img = cap.read()
+        assert cap is not None, "cap not set"
+        _, img = cap.read()
+    else:
+        img = cv2.imread('media/test_3.jpg')
 
-    scale = 4
+    return img
+
+
+def scale_image(img, scale=4):
     img = cv2.resize(img, (img.shape[1] // scale, img.shape[0] // scale))
-    cv2.imshow('original', img)
 
+    return img
+
+
+def mask_image(img):
     final_image = np.zeros(img.shape, dtype=np.uint8)
 
-    for color in ranges:
+    for color in RANGES:
         image = img.copy()
         original = image.copy()
         image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -41,47 +49,48 @@ while (True):
         mask = cv2.inRange(image, lower, upper)
         detected = cv2.bitwise_and(original, original, mask=mask)
         final_image = cv2.bitwise_or(final_image, detected)
-        # cv2.imshow(f'color {color[2]}', detected)
 
-        cv2.imshow('final image before', final_image)
-
-        blurred = cv2.GaussianBlur(final_image, (5, 5), 0)
-
-        cv2.imshow('sdkfbjkgelrtub', blurred)
-
-        cannied = cv2.Canny(blurred, threshold1=200, threshold2=600)
-        contours0, _ = cv2.findContours(cannied.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        contours = [cv2.approxPolyDP(cnt, 3, True) for cnt in contours0]
-
-        vis = np.zeros(final_image.shape, np.uint8)
-        # for i, c in enumerate(sorted(contours, key=lambda x: cv2.arcLength(x, True))[-5:]):
-        #     cv2.imshow(f"TITLE{i}", cv2.drawContours(vis.copy(), [c], 0, (255, 255, 255), 3, cv2.LINE_AA))
-
-        cv2.drawContours(vis, contours, -1, (255, 255, 255), 3, cv2.LINE_AA)
-        # print(contours)
-
-        cv2.imshow("sdfdkslklsdfjdslkfj", vis)
-
-        # lsd = cv2.createLineSegmentDetector(0)
-
-        a = cv2.threshold(cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY), 0, 240, cv2.THRESH_BINARY)[1]
-
-        cv2.imshow("A", a)
+    return final_image
 
 
-    # lines = lsd.detect(cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY))[0]
-    # cv2.imshow("sdfsdf", lsd.drawSegments(blurred, lines))
+def produce_contours(img):
+    cannied = cv2.Canny(img, threshold1=200, threshold2=600)
+    contours0, _ = cv2.findContours(cannied.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = [cv2.approxPolyDP(cnt, 3, True) for cnt in contours0]
+
+    vis = np.zeros(img.shape, np.uint8)
+
+    return cv2.drawContours(vis, contours, -1, (255, 255, 255), 3, cv2.LINE_AA)
 
 
+def fill_image(img):
+    return cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 0, 240, cv2.THRESH_BINARY)[1]
 
 
-    # cv2.imshow('final image', final_image)
+def produce_image():
+    img = get_image()
+    img = scale_image(img)
+    cv2.imshow("original", img)
 
-    # im2, contours, hierarchy = cv2.findContours(final_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    img = mask_image(img)
+    cv2.imshow("masked", img)
 
-    # convex_hull = cv2.convexHull(final_image)
-    if USE_CAM and cv2.waitKey(waitTime) & 0xFF == ord('q'):
-        break
+    blurred = cv2.GaussianBlur(img, (5, 5), 0)
+    filled = fill_image(blurred)
+    cv2.imshow("filled", filled)
+
+    contours = produce_contours(img)
+    cv2.imshow("contours", contours)
+
+
+if USE_CAM:
+    while True:
+        produce_image()
+
+        if USE_CAM and cv2.waitKey(waitTime) & 0xFF == ord('q'):
+            break
+else:
+    produce_image()
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
