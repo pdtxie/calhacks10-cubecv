05afcf64b5ad56c780325de6808583c51d88f118
Tim Xie <pdt.xie@gmail.com>
2023-10-28 23:31:09 -0700

    some progress

--- identify.py
+++ identify.py
@@ -7,10 +7,10 @@ blue = [[100, 143, 145], [118, 255, 255], 'blue']
 orange = [[0, 172, 83], [20, 255, 255], 'orange']
 red = [[170, 145, 80], [179, 255, 255], 'red']
 
-white = [[60, 60, 215], [120, 115, 255], 'white']
+# white = [[60, 60, 215], [120, 115, 255], 'white']
 yellow = [[28, 145, 0], [40, 255, 255], 'yellow']
 
-ranges = [green, blue, orange, red, white, yellow]
+ranges = [green, blue, orange, red, yellow]
 
 
 USE_CAM = True
@@ -19,7 +19,7 @@ if USE_CAM:
     cap = cv2.VideoCapture(1)
     waitTime = 330
 else:
-    img = cv2.imread('media/test_1.jpg')
+    img = cv2.imread('media/test_3.jpg')
 
 
 while (True):
@@ -43,29 +43,45 @@ while (True):
         final_image = cv2.bitwise_or(final_image, detected)
         # cv2.imshow(f'color {color[2]}', detected)
 
-    cv2.imshow('final image before', final_image)
+        cv2.imshow('final image before', final_image)
 
-    cv2.imshow("blur", cv2.GaussianBlur(final_image, (5, 5), 0))
+        blurred = cv2.GaussianBlur(final_image, (5, 5), 0)
 
-    final_image = cv2.Canny(image=cv2.GaussianBlur(final_image, (5, 5), 0), threshold1=100, threshold2=200)
+        cv2.imshow('sdkfbjkgelrtub', blurred)
 
-    cv2.imshow('final image', final_image)
+        cannied = cv2.Canny(blurred, threshold1=200, threshold2=600)
+        contours0, _ = cv2.findContours(cannied.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours = [cv2.approxPolyDP(cnt, 3, True) for cnt in contours0]
 
-# im2, contours, hierarchy = cv2.findContours(final_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        vis = np.zeros(final_image.shape, np.uint8)
+        # for i, c in enumerate(sorted(contours, key=lambda x: cv2.arcLength(x, True))[-5:]):
+        #     cv2.imshow(f"TITLE{i}", cv2.drawContours(vis.copy(), [c], 0, (255, 255, 255), 3, cv2.LINE_AA))
 
-# convex_hull = cv2.convexHull(final_image)
+        cv2.drawContours(vis, contours, -1, (255, 255, 255), 3, cv2.LINE_AA)
+        # print(contours)
+
+        cv2.imshow("sdfdkslklsdfjdslkfj", vis)
+
+        # lsd = cv2.createLineSegmentDetector(0)
+
+        a = cv2.threshold(cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY), 0, 240, cv2.THRESH_BINARY)[1]
+
+        cv2.imshow("A", a)
+
+
+    # lines = lsd.detect(cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY))[0]
+    # cv2.imshow("sdfsdf", lsd.drawSegments(blurred, lines))
+
+
+
+
+    # cv2.imshow('final image', final_image)
+
+    # im2, contours, hierarchy = cv2.findContours(final_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # convex_hull = cv2.convexHull(final_image)
     if USE_CAM and cv2.waitKey(waitTime) & 0xFF == ord('q'):
         break
 
-# Remove noise
-# kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-# opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=1)
-
-# Find contours and find total area
-# cnts = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
-# cv2.drawContours(original, cnts, -1, (0,0,0), 2)
-
-# cv2.imshow('mask', mask)
-# cv2.imshow('opening', opening)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
